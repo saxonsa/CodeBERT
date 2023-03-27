e87b482455ef0560a7475af73d8f3f01ff0dbb36
Xingyun SA <saxonsa5683@163.com>
2023-03-27 12:13:18 +0100

    change to graphcodebert

--- GraphCodeBERT/codeSummarization/script_run.sh
+++ GraphCodeBERT/codeSummarization/script_run.sh
@@ -9,6 +9,6 @@ output_dir=model/$lang
 train_file=$data_dir/$lang/train.jsonl
 dev_file=$data_dir/$lang/valid.jsonl
 epochs=20 
-pretrained_model=microsoft/codebert-base #Roberta: roberta-base
+pretrained_model=microsoft/graphcodebert-base #Roberta: roberta-base
 
 python run.py --lang $lang --do_train --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
